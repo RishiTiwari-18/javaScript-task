fc8d20dfd25b0961cc24389fb40dd077066c79ff
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-09-10 16:39:49 +0530

    Task 7 completed

---
+++ Task 7/script.js
@@ -0,0 +1,16 @@
+let progress = document.querySelector(".progress")
+let info = document.querySelector("h3")
+let intervel;
+let count = 0
+
+intervel = setInterval(function(){
+    if(count === 100){
+        clearInterval(intervel)
+    info.style.display = "block"
+
+    }else{
+            count++;
+    progress.style.width = count + "%"
+    }
+
+}, 100)
